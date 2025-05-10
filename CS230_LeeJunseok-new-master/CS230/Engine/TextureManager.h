fb8f4576a0c2dec1bc63d50ee019d299fb030fff
hannibal <50999631+ggguhfyj@users.noreply.github.com>
2025-05-10 21:35:09 +0900

    latest version

---
+++ CS230_LeeJunseok-new-master/CS230/Engine/TextureManager.h
@@ -0,0 +1,26 @@
+/*
+Copyright (C) 2023 DigiPen Institute of Technology
+Reproduction or distribution of this file or its contents without
+prior written consent is prohibited
+File Name:  TextureManager.h
+Project:    CS230 Engine
+Author:     Jonathan Holmes junseok lee
+Created:    March 8, 2023
+*/
+
+#pragma once
+#include <filesystem>
+#include <map>
+
+namespace CS230 {
+    class Texture;
+
+    class TextureManager {
+    public:
+        Texture* Load(const std::filesystem::path& file_name);
+        void Unload();
+
+    private:
+        std::map<std::filesystem::path, Texture*> textures;
+    };
+}
